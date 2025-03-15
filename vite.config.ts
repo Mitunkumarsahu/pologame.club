c209e598e05da752aa0a8d7f47c2697de0fbc11b
Mitunkumarsahu <mitunkumarsahu@gmail.com>
2025-03-15 21:14:55 +0530

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  assetsInclude: ['**/*.mp4', '**/*.webm', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.ico', '**/*.webp', '**/*.avif', '**/*.pdf', '**/*.zip', '**/*.rar', '**/*.7z', '**/*.tar', '**/*.gz', '**/*.bz2', '**/*.xz', '**/*.exe', '**/*.msi', '**/*.dmg', '**/*.pkg', '**/*.deb', '**/*.rpm', '**/*.apk', '**/*.ipa', '**/*.appx', '**/*.xap', '**/*.jar', '**/*.war', '**/*.ear', '**/*.swf', '**/*.flv', '**/*.f4v', '**/*.ogv', '**/*.ogg', '**/*.webm', '**/*.mp3', '**/*.wav', '**/*.flac', '**/*.aac', '**/*.m4a', '**/*.opus', '**/*.ttf', '**/*.otf', '**/*.woff', '**/*.woff2', '**/*.eot', '**/*.sfnt', '**/*.zip', '**/*.rar', '**/*.7z', '**/*.tar', '**/*.gz', '**/*.bz2', '**/*.xz', '**/*.exe', '**/*.msi', '**/*.dmg', '**/*.pkg', '**/*.deb', '**/*.rpm', '**/*.apk', '**/*.ipa', '**/*.appx', '**/*.xap', '**/*.jar', '**/*.war', '**/*.ear', '**/*.swf', '**/*.flv', '**/*.f4v', '**/*.ogv', '**/*.ogg', '**/*.webm', '**/*.mp3', '**/*.wav', '**/*.flac', '**/*.aac', '**/*.m4a', '**/*.opus', '**/*.ttf', '**/*.otf', '**/*.woff', '**/*.woff2', '**/*.eot', '**/*.sfnt', '**/*.zip', '**/*.rar', '**/*.7z', '**/*.tar', '**/*.gz', '**/*.bz2', '**/*.xz', '**/*.exe', '**/*.msi', '**/*.dmg', '**/*.pkg', '**/*.deb', '**/*.rpm', '**/*.apk', '**/*.ipa', '**/*.appx', '**/*.xap', '**/*.jar', '**/*.war', '**/*.ear', '**/*.swf', '**/*.flv', '**/*.f4v', '**/*.ogv', '**/*.ogg']
+});
